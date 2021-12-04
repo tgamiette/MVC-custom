f15de333f211ea8ee96738eed5e458b5ebba8eeb
teddy <tgamiette@kernix.com>
2021-12-04 16:41:29 +0100

    .

--- app/entity/Post.php
+++ app/entity/Post.php
@@ -4,5 +4,24 @@ namespace App\entity;
 
 class Post
 {
+    private string $description;
+    private Authors $user;
+
+    /**
+     * @return Authors
+     */
+    public function getUser(): Authors
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param Authors $user
+     */
+    public function setUser(Authors $user): void
+    {
+        $this->user = $user;
+    }
+
 
 }

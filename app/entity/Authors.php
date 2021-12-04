77909bfd47f0ad3ca2693ac5441bae56b95d403a
teddy <tgamiette@kernix.com>
2021-12-04 16:41:49 +0100

    .

---
+++ app/entity/Authors.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\entity;
+
+class Authors
+{
+    private string $firstName;
+    private string $lastName;
+
+    /**
+     * @return string
+     */
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $firstName
+     */
+    public function setFirstName(string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $lastName
+     */
+    public function setLastName(string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+
+}

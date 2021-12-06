b1195bfa06c2a8fa2cce5bb7742baf5150cb0c54
teddy <tgamiette@kernix.com>
2021-12-06 12:11:16 +0100

    correctif bindvalue

--- App/Manager/PostManager.php
+++ App/Manager/PostManager.php
@@ -9,27 +9,26 @@ class PostManager extends BaseManager
 
     public function getAllPosts()
     {
-        $query = $this->db->query('SELECT * FROM post');;
+        $query = $this->db->query("SELECT * FROM post");;
         $query->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, 'Entity\Post.php');
-        
+
         return $query->fetchAll();
     }
 
     public function getPostById(int $id)
     {
         $query = $this->db->prepare('SELECT * FROM post WHERE id = :id');
-        $query =bindValue(':id', $id, \PDO::PARAM_INT);
+        $query->bindValue(':id', $id, \PDO::PARAM_INT);
         $query->execute();
         $query->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, 'Entity\Post.php');
-        
-        return $query->fetch();
 
+        return $query->fetch();
     }
 
     public function deletePost(int $id)
     {
         $query = $this->db->prepare('DELETE FROM post WHERE id = :id');
-        $query =bindValue(':id', $id, \PDO::PARAM_INT);
+        $query->bindValue(':id', $id, \PDO::PARAM_INT);
         $query->execute();
     }
 

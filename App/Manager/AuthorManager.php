06c9e63ee1cd138a571902fe09c25ec55666eda9
Mehdi Saoud <saoudmehdi5@gmail.com>
2021-12-08 15:12:55 +0100

    Add AdminController | Add userlist route | Add method to AuthorManager

--- App/Manager/AuthorManager.php
+++ App/Manager/AuthorManager.php
@@ -4,7 +4,25 @@ namespace App\Manager;
 
 class AuthorManager extends BaseManager
 {
-    public function findAll() :array{
-        
+    public function findAll() :array
+    {
+
+        $query = $this->db->query('SELECT * FROM author');
+        $usersData = $query->fetchAll(\PDO::FETCH_ASSOC);
+        $users = [];
+        foreach ($usersData as $user) {
+            $users[] = new Author($user);
+        }
+
+        return $users;
+    }
+
+    public function isAdmin() :bool
+    {
+        $query = $this->db->pepare('SELECT isAdmin FROM author WHERE id = :id');
+        $query->bindValue(':id', $_SESSION('id'), \PDO::PARAM_INT);
+        $query->execute();
+
+        return $query->fetch();
     }
 }

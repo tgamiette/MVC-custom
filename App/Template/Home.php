bac86582cefa59f9e8340c452a94a92e947ce18e
Mehdi Saoud <saoudmehdi5@gmail.com>
2021-12-09 14:36:34 +0100

    add working connexion

--- App/Template/Home.php
+++ App/Template/Home.php
@@ -1,5 +1,10 @@
 <pre
 <?php
 
-//var_dump($args);
-var_dump($post);
+var_dump($posts[0]);
+
+printf($posts[0]->getId());
+
+?>
+
+<a href="?p=post&id=<?php echo($posts[0]->getId()) ?>">Voir cette arcticle</a>
--- App/Template/_navbar.php
+++ App/Template/_navbar.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Login dropdown</a>
+            <a class="navbar-brand" href="?p=singin">Login dropdown</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->

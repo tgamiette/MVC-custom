5474739cdd3408cfdce0217248c5d2b8bdbaf20c
lucalg322 <luca.grousset@gmail.com>
2021-12-10 13:17:31 +0100

    update home

--- App/Template/Home.php
+++ App/Template/Home.php
@@ -5,7 +5,7 @@
 
         <div class="w-25 my-2"><h3><?php echo($post->getTitle()) ?></h3>
         </div>
-        <div class="my-2"><p><?php echo($post->getContent()) ?> </p</div>
+        <div class="my-2" ><p style=" text-overflow: ellipsis ;white-space: nowrap;overflow: hidden"><?php echo($post->getContent()) ?> </p</div>
         <div style="float: left"> <a href="post/<?php echo($post->getId()) ?>">Voir cette arcticle</a> </div>
     <div style="float: right; padding-left:10px;"><?php echo($post->getpublishedAt()) ?></div><div style="float: right">  <?php echo($post->getAuthor()->getfirstName()) ?> <?php echo($post->getAuthor()->getlastName()) ?></div>
         <br>

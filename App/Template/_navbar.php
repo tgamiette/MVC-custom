f50e2aeedb45b04ffbaf2a40b4dda161be252af8
lucalg322 <luca.grousset@gmail.com>
2021-12-10 12:13:54 +0100

    add navbar

--- App/Template/_navbar.php
+++ App/Template/_navbar.php
@@ -1,18 +1,31 @@
-<nav class="navbar navbar-default navbar-inverse" role="navigation">
-    <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <?php
-            if ( !(new \App\Framework\Session\Session())->get("id")) : ?>
-                <a class="navbar-brand" href="singin">Login</a>
-            <?
-            endif; ?>
-            <?php
-            if ( (new \App\Framework\Session\Session())->get("id")) : ?>
-                <a class="navbar-brand" href="logout">Logout</a>
-            <?
-            endif; ?>
+ <div class="navbar navbar-dark bg-dark">
+            <a href="/" class="btn btn-secondary mx-2">Home</a>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                  Api
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                    <li><a class="dropdown-item" href="api/post" target="_blank">Post</a></li>
+                    <li><a class="dropdown-item" href="api/comment" target="_blank">Comment</a></li>
 
+                </ul>
+            </div>
+            <a href="/" class="btn btn-secondary mx-2">Créer un Post</a>
+            <div>
+                <?php
+                if ( !(new \App\Framework\Session\Session())->get("id")) : ?>
+                    <a class="btn btn-secondary mx-2" href="singin">Login</a>
+                <?
+                endif; ?>
+                <?php
+                if ( (new \App\Framework\Session\Session())->get("id")) : ?>
+                    <a class="btn btn-secondary mx-2" href="logout">Logout</a>
+                <?
+                endif; ?>
+
+            </div>
         </div>
-    </div>
-</nav>
+
+
+
+

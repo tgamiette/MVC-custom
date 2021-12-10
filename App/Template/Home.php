10c3cbd26dc1949c035ed347410dfaddcc347bf7
lucalg322 <luca.grousset@gmail.com>
2021-12-10 10:40:09 +0100

    update Home.php

--- App/Template/Home.php
+++ App/Template/Home.php
@@ -6,7 +6,7 @@
         <div class="w-25 my-2"><h3><?php echo($post->getTitle()) ?></h3>
         </div>
         <div class="my-2"><p><?php echo($post->getContent()) ?> </p</div>
-        <div style="float: left"> <a href="?p=post&id=<?php echo($post->getId()) ?>">Voir cette arcticle</a> </div>
+        <div style="float: left"> <a href="post/<?php echo($post->getId()) ?>">Voir cette arcticle</a> </div>
     <div style="float: right; padding-left:10px;"><?php echo($post->getpublishedAt()) ?></div><div style="float: right">  <?php echo($post->getAuthor()->getfirstName()) ?> <?php echo($post->getAuthor()->getlastName()) ?></div>
         <br>
     <?php } ?>

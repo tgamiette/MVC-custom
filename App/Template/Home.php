59a0b87e006a75fb946160217c96cf655a930e19
lucalg322 <luca.grousset@gmail.com>
2021-12-10 14:42:51 +0100

    update Home.php & add Show.php

--- App/Template/Home.php
+++ App/Template/Home.php
@@ -7,8 +7,10 @@
         </div>
         <div class="my-2" ><p style=" text-overflow: ellipsis ;white-space: nowrap;overflow: hidden"><?php echo($post->getContent()) ?> </p</div>
         <div style="float: left"> <a href="post/<?php echo($post->getId()) ?>">Voir cette arcticle</a> </div>
-    <div style="float: right; padding-left:10px;"><?php echo($post->getpublishedAt()) ?></div><div style="float: right">  <?php echo($post->getAuthor()->getfirstName()) ?> <?php echo($post->getAuthor()->getlastName()) ?></div>
-        <br>
+        <div style="float: right; padding-left:10px;"><?php echo($post->getpublishedAt()) ?></div><div style="float: right">  <?php echo($post->getAuthor()->getfirstName()) ?> <?php echo($post->getAuthor()->getlastName()) ?></div>
+
+        <br><br>
+        <div style="border-bottom: 2px solid; margin-left:25%;width: 50%;"></div>
     <?php } ?>
 
 </div>
